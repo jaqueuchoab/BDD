5de7ea181d67b5c6dd6098b8e308ea93142eaa5b
jaqueuchoab <jaqueline.uchoab@gmail.com>
2025-07-10 16:22:47 -0300

    App Funcional

--- DoceFidelidade/Program.cs
+++ DoceFidelidade/Program.cs
@@ -8,6 +8,7 @@ public class Program {
     Administrator admin = null;
     ClientService clientService = new ClientService();
     FeedbackService feedbackService = new FeedbackService();
+    AdministratorService adminService = new AdministratorService();
     int option = 0;
 
     do {
@@ -15,10 +16,11 @@ public class Program {
       Console.WriteLine("Bem-vindo ao Doce Fidelidade!");
       Console.WriteLine("O que você deseja fazer?");
       Console.WriteLine("1. Cadastrar Cliente");
-      Console.WriteLine("2. Listar Clientes (Apenas Administrador)");
-      Console.WriteLine("3. Deixar Feedback");
-      Console.WriteLine("4. Listar Feedback (Apenas Administrador)");
-      Console.WriteLine("5. Sair\n");
+      Console.WriteLine("2. Cadastrar Administrador");
+      Console.WriteLine("3. Listar Clientes (Administrador)");
+      Console.WriteLine("4. Listar Feedback (Administrador)");
+      Console.WriteLine("5. Deixar Feedback (Cliente)");
+      Console.WriteLine("6. Sair\n");
       Console.WriteLine("----------------------------------------");
 
       Console.Write("Digite a opção desejada: ");
@@ -30,27 +32,54 @@ public class Program {
           Console.WriteLine("\nPor favor, preencha os dados para o cadastro:");
 
           Console.Write("Nome: ");
-          string name = Console.ReadLine();
+          string nameClient = Console.ReadLine();
 
           Console.Write("Email: ");
-          string email = Console.ReadLine();
+          string emailClient = Console.ReadLine();
 
           Console.Write("Telefone: ");
-          string phone = Console.ReadLine();
+          string phoneClient = Console.ReadLine();
 
-          client = new Client(name, email, phone);
+          client = new Client(nameClient, emailClient, phoneClient);
           clientService.AddClient(client);
 
           Console.WriteLine("Cadastro realizado com sucesso!");
           Console.WriteLine("----------------------------------------");
           break;
         case 2:
-          if (clientService.GetListClients().Count == 0) {
-            Console.WriteLine("\nNenhum cliente cadastrado. Por favor, cadastre um cliente primeiro.");
-          }  
-          
+          Console.WriteLine("\nPor favor, preencha os dados para o cadastro:");
+
+          Console.Write("Nome: ");
+          string nameAdm = Console.ReadLine();
+
+          Console.Write("Email: ");
+          string emailAdm = Console.ReadLine();
+
+          Console.Write("Telefone: ");
+          string phoneAdm = Console.ReadLine();
+
+          Console.Write("Senha: ");
+          string senhaAdm = Console.ReadLine();
+
+          admin = new Administrator(nameAdm, emailAdm, phoneAdm, senhaAdm);
+          adminService.AddAdmin(admin);
+
+          Console.WriteLine("Cadastro realizado com sucesso!");
+          Console.WriteLine("----------------------------------------");
+          break;
+        case 3:
           if(admin.IsAdmin){
+            Console.WriteLine("\nAdministrador, digite sua senha");
+            string senha = Console.ReadLine();
+            if(admin.Password != senha) {
+              Console.WriteLine("\nSenha incorreta. Acesso negado.");
+              break;
+            }
+
             Console.WriteLine("\nLista de Clientes:");
+              if (clientService.GetListClients().Count == 0) {
+              Console.WriteLine("\nNenhum cliente cadastrado. Por favor, cadastre um cliente primeiro.");
+              }  
             foreach (var c in clientService.GetListClients()) {
               Console.WriteLine($"ID: {c.Id}, Nome: {c.Name}, Email: {c.Email}, Telefone: {c.Phone}\n");
             }
@@ -58,7 +87,25 @@ public class Program {
             Console.WriteLine("\nApenas administradores podem listar clientes");
           }
           break;
-        case 3:
+        case 4:
+          if(admin.IsAdmin) {
+            Console.WriteLine("\nAdministrador, digite sua senha");
+            string senha = Console.ReadLine();
+            if(admin.Password != senha) {
+              Console.WriteLine("\nSenha incorreta. Acesso negado.");
+              break;
+            }
+
+            if(feedbackService.GetListFeedbacks().Count == 0) {
+              Console.WriteLine("\nNenhum feedback disponível.");
+              break;
+            } 
+
+            Console.WriteLine("\nLista de Feedbacks:");
+            feedbackService.DisplayFeedbacks();
+          }
+          break;
+        case 5:
           if(client == null) {
             Console.WriteLine("\nNenhum cliente cadastrado. Por favor, cadastre um cliente primeiro.");
             break;
@@ -70,17 +117,7 @@ public class Program {
           int nota = int.Parse(Console.ReadLine());
 
           string success = feedbackService.CreateFeedback(client, mensagem, nota);
-          Console.WriteLine(success);
-          break;
-        case 4:
-          if(feedbackService.GetListFeedbacks().Count == 0) {
-            Console.WriteLine("\nNenhum feedback disponível.");
-          } 
-          
-          if(admin.IsAdmin) {
-            Console.WriteLine("\nLista de Feedbacks:");
-            feedbackService.DisplayFeedbacks();
-          }
+          Console.WriteLine("Feedback enviado com sucesso!");
           break;
         default:
           Console.WriteLine("Opção inválida. Por favor, tente novamente.");
